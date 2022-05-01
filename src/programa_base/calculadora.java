75530f6f85fa0c68a1140456a500939f13433c2d
Eric Fernandez <efernandezalbuq@gmail.com>
2022-05-01 02:35:36 -0300

    + Funções & Testes

--- src/programa_base/calculadora.java
+++ src/programa_base/calculadora.java
@@ -1,21 +1,44 @@
 package programa_base;
 
-import java.util.Scanner; 
 
 public class calculadora {
 	
-	public int somar(int valor1, int valor2) {
-		Scanner scanner = new Scanner (System.in);
-		
-		System.out.println("Valor 01: ");
-		valor1 = scanner.nextInt();
-		System.out.println("Valor 02: ");
-		valor2 = scanner.nextInt();
-		int soma = valor1 + valor2;
-		
-		scanner.close();
-		
+	public static float soma(float valor1, float valor2) {
+		float soma = valor1 + valor2;
 		return soma;
 	}
-
+	
+	public static float subtracao(float valor1, float valor2) {
+		float subtracao = valor1 - valor2;
+		return subtracao;
+	}
+	
+	public static float multiplicacao(float valor1, float valor2) {
+		float multiplicacao = valor1 * valor2;
+		return multiplicacao;
+	}
+	
+	public static float divisao(float valor1, float valor2) {
+		float divisao = valor1 / valor2;
+		return divisao;
+	}
+	
+	public static float potenciacao(float valor1, float valor2) {
+		float potencia = (float) Math.pow(valor1, valor2);
+		return potencia;
+	}
+	
+	public static float radiciacao(float valor) {
+		float raiz = (float) Math.sqrt(valor);
+		return raiz;
+	}
+	
+	public static int fatorial(int valor) {
+		int fatorial = 1;
+		do {
+			fatorial = fatorial * valor;
+			valor = valor - 1;
+		} while (valor >= 1);
+		return fatorial;
+	}
 }
--- src/programa_base/calculadoraTest.java
+++ src/programa_base/calculadoraTest.java
@@ -7,12 +7,73 @@ import org.junit.jupiter.api.Test;
 class calculadoraTest {
 
 	@Test
-	void testSomar() {
-		calculadora calc = new calculadora();
-		int x = 0;
-		int y = 0;
-		int soma = calc.somar(x, y);
-		assertEquals(3, soma);
+	void testSoma() {
+		float n1 = 10;
+		float n2 = 5;
+		float rEsperado = 15;
+		float rReal = calculadora.soma(n1, n2);
+		assertEquals(rReal, rEsperado);
+	}
+	
+	@Test
+	void test2Soma() {
+		float n1 = -10;
+		float n2 = 5;
+		float rEsperado = -5;
+		float rReal = calculadora.soma(n1, n2);
+		assertEquals(rReal, rEsperado);
+	}
+
+	@Test
+	void testSubtracao() {
+		float n1 = 10;
+		float n2 = 5;
+		float rEsperado = 5;
+		float rReal = calculadora.subtracao(n1, n2);
+		assertEquals(rReal, rEsperado);
+	}
+
+	@Test
+	void testMultiplicacao() {
+		float n1 = 10;
+		float n2 = 5;
+		float rEsperado = 50;
+		float rReal = calculadora.multiplicacao(n1, n2);
+		assertEquals(rReal, rEsperado);
+	}
+
+	@Test
+	void testDivisao() {
+		float n1 = 10;
+		float n2 = 5;
+		float rEsperado = 2;
+		float rReal = calculadora.divisao(n1, n2);
+		assertEquals(rReal, rEsperado);
+	}
+
+	@Test
+	void testPotenciacao() {
+		float n1 = 10;
+		float n2 = 5;
+		float rEsperado = 100000;
+		float rReal = calculadora.potenciacao(n1, n2);
+		assertEquals(rReal, rEsperado);
+	}
+
+	@Test
+	void testRadiciacao() {
+		float n = 9;
+		float rEsperado = 3;
+		float rReal = calculadora.radiciacao(n);
+		assertEquals(rReal, rEsperado);
+	}
+
+	@Test
+	void testFatorial() {
+		int n = 5;
+		int rEsperado = 120;
+		int rReal = calculadora.fatorial(n);
+		assertEquals(rReal, rEsperado);
 	}
 
 }
